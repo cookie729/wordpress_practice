2dadcf61af1966d848728a424a28e09b4715c608
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-10-29 23:22:53 +0900

    fix: コードの修正

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 function add_files() {
   wp_enqueue_style('reset-style', 'https://unpkg.com/destyle.css@2.0.2/destyle.css');
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family-Noto+Sans+JP:wght@400;700&display=swap');
-  wp_enqueue_style('main-style', get_stylesheet_url());
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
+  wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'add_files');
 
