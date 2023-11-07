96de25d4d4adf466fa34cb56c9cbcb9362c67ff7
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-11-07 22:44:22 +0900

    feat: create ahcive

--- archive.php
+++ archive.php
@@ -1,4 +1,8 @@
 <?php get_header(); ?>
+    <div class="wrapper">
+      <!-- アーカイブタイトル -->
+      <h1 class="page-title"><?php the_archive_title(); ?></h1>
+    </div>
     <main class="main-contents wrapper">
         <div class="post-list">
         <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
