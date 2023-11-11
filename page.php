86c24744fca18817184098d295218abf1a1a4b50
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-11-11 23:48:37 +0900

    end

---
+++ page.php
@@ -0,0 +1,27 @@
+<!-- header の追加 -->
+<?php get_header(); ?>
+
+    <main class="main-contents wrapper">
+        <div class="post-list">
+        <!-- ループ処理 -->
+          <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <article class="post-item">
+            <?php if(has_post_thumbnail()): ?>
+              <?php the_post_thumbnail('medium_large'); ?>
+            <?php endif; ?>
+            <header class="post-header">
+                <h1 class="post-title">
+                    <?php the_title(); ?>
+                </h1>
+            </header>
+            <div class="post-content wrapper">
+                <?php the_content(); ?>
+            </div>
+          </article>
+          <?php endwhile; else : ?>
+            <p>記事はありません。</p>
+          <?php endif; ?>
+      </div>
+    </main>
+<!-- footer の追加 -->
+<?php get_footer(); ?>
